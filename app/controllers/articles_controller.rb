b86a5270038ba638682efafecf95b0e19f0a780f
doamaral <lopesdoamaral@gmail.com>
2018-02-23 12:44:12 -0300

    Added Category to forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,12 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    if params[:category].blank?
+      @articles = Article.all
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @articles  = Article.where(category_id: @category_id)
+    end
   end
 
   # GET /articles/1
@@ -71,6 +76,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :body, :image_url)
+      params.require(:article).permit(:title, :body, :image_url, :category_id)
     end
 end
