549d0cb2e9e8fdd10836b52ef28c03e57e61b1c0
doamaral <lopesdoamaral@gmail.com>
2018-02-23 12:26:26 -0300

    Category Migrations and Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222200740) do
+ActiveRecord::Schema.define(version: 20180223151836) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20180222200740) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
